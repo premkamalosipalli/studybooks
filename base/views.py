761540f2487b17fabf8bce222516cf22646d00b1
premkamalosipalli <kamalosipalli@gmail.com>
2024-07-20 17:22:25 -0500

    added login and logout

--- base/views.py
+++ base/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render, redirect
 from .models import Room, Topic
 from .forms import RoomForm
 from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import  messages
 
 # Create your views here.
 
@@ -13,6 +16,30 @@ from django.db.models import Q
 #     {'id':4, 'name': 'Lets Generate BackEnd!'},
 # ]
 
+def loginPage(request):
+
+    if request.method =='POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, "User Does Not Exists")
+        
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, "Username or password does not exists..")
+    context = {}
+    return render(request, 'base/login_register.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
     rooms = Room.objects.filter(

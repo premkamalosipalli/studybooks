0b6a85969555cf016e15dfdfa5f20e6186ddb5cf
premkamalosipalli <kamalosipalli@gmail.com>
2024-07-23 10:54:28 -0500

    udpated static files dirs

--- studybooks/settings.py
+++ studybooks/settings.py
@@ -139,14 +139,14 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
 
-# STATICFILES_DIRS = [
-#     BASE_DIR / 'static'
-# ]
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
 
 import os
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field

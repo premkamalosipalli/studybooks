6dccf94d6c1ba81fa3be080a8e7702ee61d97d93
premkamalosipalli <kamalosipalli@gmail.com>
2024-07-23 10:17:38 -0500

    added static root in settings

--- studybooks/settings.py
+++ studybooks/settings.py
@@ -145,8 +145,8 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
 
-
-# STATIC_ROOT =
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
